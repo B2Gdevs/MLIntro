6908b24b99cfd80e11271ebfa38571e425ca1510
ben <benjamingarrard5279@gmail.com>
2019-05-13 18:18:23 -0500

    adding node with for of loop

--- js/heartDisease.js
+++ js/heartDisease.js
@@ -26,6 +26,6 @@ let csvArray = parseCSV(csvText);
 let headers = popHeaders(csvArray);
 let dataContainer = document.getElementById("data-container");
 
-// csvArray.forEach((row) => {
-//   dataContainer.appendChild(createPatientDescription(row));
-// });
+for(let row of csvArray){
+  document.appendChild(createPatientDescription(row));
+}
