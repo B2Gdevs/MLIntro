827984f73215d0ba2bd7f4363af5bdc2360ba02e
ben <benjamingarrard5279@gmail.com>
2019-05-13 18:27:58 -0500

    dom still doesnt seem to be loading content

--- js/heartDisease.js
+++ js/heartDisease.js
@@ -29,8 +29,9 @@ document.addEventListener("DOMContentLoaded", () =>{
   let headers = popHeaders(csvArray);
   let dataContainer = document.getElementById("data-container");
 
-  csvArray.forEach((row) => {
-    dataContainer.appendChild(createPatientDescription(row));
-  });
+// csvArray.forEach((row) => {
+//   dataContainer.appendChild(createPatientDescription(row));
+// });
 
+  dataContainer.appendChild(document.createElement("div"));
 });
